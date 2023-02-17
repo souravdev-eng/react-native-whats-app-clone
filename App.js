6a1d81acbfc9274d1231bb0651b923d4e96508cd
souravdev-eng <sourav.m@handajitech.com>
2023-02-17 18:03:32 +0530

    Setting up aws configaration

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
+import { Amplify } from 'aws-amplify';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View, StatusBar as St } from 'react-native';
 import Navigator from './src/navigation';
+import awsconfig from './src/aws-exports';
+
+Amplify.configure(awsconfig);
 
 export default function App() {
   return (
